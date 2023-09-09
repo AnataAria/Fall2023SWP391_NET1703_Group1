ee8aef384e9a84a26c7aef8793197ea2e151b062
Anata Arisa <kakata28419@gmail.com>
2023-09-09 12:55:42 +0700

    Init Spring Boot API

---
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/DrawingcousesellingApplication.java
@@ -0,0 +1,13 @@
+package com.group1.drawingcouseselling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingcousesellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawingcousesellingApplication.class, args);
+	}
+
+}
---
+++ api/drawingcouseselling/src/test/java/com/group1/drawingcouseselling/DrawingcousesellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.drawingcouseselling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingcousesellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
