82f87cb1611dfaa2cfbda1dc1f3b20e6cf124ddb
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2023-09-14 23:18:19 +0700

    Add user not found exception

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/UserNotFoundException.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/UserNotFoundException.java
@@ -1,5 +1,7 @@
 package com.group1.drawingcouseselling;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException (String message){
         super(message);
