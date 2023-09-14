0312dc75672721db0a9cf3f09df53e01971a4039
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2023-09-14 21:32:22 +0700

    Add existed user exception

---
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/ExistedUserException.java
@@ -0,0 +1,7 @@
+package com.group1.drawingcouseselling;
+
+public class ExistedUserException extends RuntimeException{
+    public ExistedUserException (String message){
+        super(message);
+    }
+}
