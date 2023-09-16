fc718c56bcece85dc7adb4e5d1ece0a3c08c806b
Anata Arisa <kakata28419@gmail.com>
2023-09-16 14:37:13 +0700

    Add PaypalConfig

---
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/config/PaypalConfig.java
@@ -0,0 +1,37 @@
+package com.group1.drawingcouseselling.config;
+
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class PaypalConfig {
+    @Value("${paypal.client.id}")
+    private String clientId;
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+    @Value("${paypal.mode}")
+    private String mode;
+    @Bean
+    public Map<String, String> paypalSdkConfig(){
+        Map<String,String> config = new HashMap<>();
+        config.put("mode",mode);
+        return config;
+    }
+    @Bean
+    public OAuthTokenCredential oAuthTokenCredential(){
+        return new OAuthTokenCredential(clientId,clientSecret,paypalSdkConfig());
+    }
+
+    public APIContext apiContext() throws PayPalRESTException {
+        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
+        context.setConfigurationMap(paypalSdkConfig());
+        return context;
+    }
+}
