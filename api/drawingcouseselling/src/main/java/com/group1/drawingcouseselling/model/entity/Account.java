5aeba21bca8965c6d42fc461dd5882dd88728f28
Anata Arisa <kakata28419@gmail.com>
2023-09-16 14:21:22 +0700

    Fix Account Entity

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/model/entity/Account.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/model/entity/Account.java
@@ -1,5 +1,6 @@
 package com.group1.drawingcouseselling.model.entity;
 
+import com.group1.drawingcouseselling.model.enums.ERole;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +8,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.Date;
 
-@Entity
+@Entity(name = "account")
 @NoArgsConstructor
 public class Account implements Serializable {
     @Id
@@ -19,11 +20,68 @@ public class Account implements Serializable {
     @Column(name = "password", nullable = false)
     private String encodePassword;
     @Column(name="role", nullable = false)
-    private int role;
-    @Column(name="create_date", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    private ERole role;
+    @Column(name="create_date", nullable = false, columnDefinition = "date")
     private Date createDate;
     @Column(name = "active", nullable = false)
     private Boolean isActive;
     @Column(name="status")
     private Boolean status;
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncodePassword() {
+        return encodePassword;
+    }
+
+    public void setEncodePassword(String encodePassword) {
+        this.encodePassword = encodePassword;
+    }
+
+    public ERole getRole() {
+        return role;
+    }
+
+    public void setRole(ERole role) {
+        this.role = role;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
