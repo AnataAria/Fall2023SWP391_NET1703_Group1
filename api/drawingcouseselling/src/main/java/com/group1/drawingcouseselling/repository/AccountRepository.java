c88b543d5189f6fa094978e2c5be88d4bd758bb2
Anata Arisa <kakata28419@gmail.com>
2023-09-16 14:31:38 +0700

    Add searchbystatus and searchbyemail

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/repository/AccountRepository.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/repository/AccountRepository.java
@@ -2,10 +2,17 @@ package com.group1.drawingcouseselling.repository;
 
 import com.group1.drawingcouseselling.model.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.util.Collection;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, BigDecimal> {
+    @Query("SELECT u FROM account u WHERE u.email LIKE %:email%")
+    public Collection<Account> findAccountByEmail(@Param("email") String email);
+    @Query("SELECT u FROM account u WHERE u.status = :status")
+    public Collection<Account> findAccountsByStatus(@Param("status") String status);
 }
