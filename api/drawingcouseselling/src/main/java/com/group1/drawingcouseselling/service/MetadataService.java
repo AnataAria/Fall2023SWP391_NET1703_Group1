390042faa9f6e4cdea014400f144e814f28a6b2c
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2023-09-20 19:57:59 +0700

    Add Metadata service

---
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/MetadataService.java
@@ -0,0 +1,14 @@
+package com.group1.drawingcouseselling.service;
+
+import com.amazonaws.services.s3.model.S3Object;
+import com.group1.drawingcouseselling.model.entity.FileMeta;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface MetadataService {
+    public void upload(MultipartFile file) throws IOException;
+    public S3Object download (int id);
+    public List<FileMeta> list();
+}
