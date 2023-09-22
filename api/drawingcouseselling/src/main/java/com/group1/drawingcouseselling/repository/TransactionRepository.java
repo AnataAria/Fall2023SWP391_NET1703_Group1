fc365a4e6ab4817a3456e672a98d6d634fcd64c8
Anata Arisa <kakata28419@gmail.com>
2023-09-22 10:50:05 +0700

    vinh/add transaction repo

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/repository/TransactionRepository.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/repository/TransactionRepository.java
@@ -4,10 +4,11 @@ import com.group1.drawingcouseselling.model.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.Collection;
-
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, BigDecimal> {
     @Query("select a from transaction a where a.customer.id = :customerID")
     public Collection<Transaction> searchTransactionsCustomerID(@Param("customerID") BigDecimal customerID);
