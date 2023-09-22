5c122c956f5bd61b8ed3e01a4e947c7f5152e0d3
Anata Arisa <kakata28419@gmail.com>
2023-09-22 10:49:39 +0700

    vinh/fix entity and add exception for instructor

---
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/exception/InstructorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.group1.drawingcouseselling.exception;
+
+public class InstructorNotFoundException extends BaseException{
+    public InstructorNotFoundException(String message) {
+        super(message);
+    }
+}
