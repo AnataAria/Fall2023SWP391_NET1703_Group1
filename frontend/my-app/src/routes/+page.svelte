4fad360698cfb540d6b87c10745f25c12f973660
thanlong421999 <thanlong421999@gmail.com>
2023-09-23 00:50:26 +0700

    adding header

--- frontend/my-app/src/routes/+page.svelte
+++ frontend/my-app/src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <script>
   import { onMount } from 'svelte';
   import Nested from './Nested.svelte';
+  import Top from './Top.svelte';
 
   // Add this to initialize the Bootstrap tooltip
   onMount(() => {
    
   });
 </script>
-
+<Top />
 <p>These styles...</p>
 <Nested />
+
 <style>
 	p {
 		color: purple;
