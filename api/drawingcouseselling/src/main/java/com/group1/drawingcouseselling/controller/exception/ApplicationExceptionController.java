c3a87afcb76ee02aa7cb45da3b635f523f3b1f59
Anata Arisa <kakata28419@gmail.com>
2023-09-22 17:37:43 +0700

    vinh/custom exception

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/controller/exception/ApplicationExceptionController.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/controller/exception/ApplicationExceptionController.java
@@ -1,6 +1,9 @@
 package com.group1.drawingcouseselling.controller.exception;
 
+import com.group1.drawingcouseselling.exception.CourseMissMatchException;
+import com.group1.drawingcouseselling.exception.EmailIsMatchedException;
 import com.group1.drawingcouseselling.exception.ExistedUserException;
+import com.group1.drawingcouseselling.exception.InstructorNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,9 +12,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApplicationExceptionController {
+    @ExceptionHandler(CourseMissMatchException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> existedUserException(CourseMissMatchException courseMissMatchException){
+        return new ResponseEntity<>(courseMissMatchException.getErrorMessage(),
+                courseMissMatchException.getErrorMessage().getStatus());
+    }
+
+    @ExceptionHandler(EmailIsMatchedException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> emailIsMatchException(EmailIsMatchedException exception){
+        return new ResponseEntity<>(exception.getErrorMessage(), exception.getErrorMessage().getStatus());
+    }
+
     @ExceptionHandler(ExistedUserException.class)
-    @ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
-    public ResponseEntity<?> existedUserException(ExistedUserException existedUserException){
-        return new ResponseEntity<>(existedUserException, HttpStatus.NOT_ACCEPTABLE);
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> existUserException(ExistedUserException exception){
+        return new ResponseEntity<>(exception.getErrorMessage(), exception.getErrorMessage().getStatus());
+    }
+
+    @ExceptionHandler(InstructorNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ResponseEntity<?> instructorNotFoundException(InstructorNotFoundException exception){
+        return new ResponseEntity<>(exception.getErrorMessage(), exception.getErrorMessage().getStatus());
     }
 }
