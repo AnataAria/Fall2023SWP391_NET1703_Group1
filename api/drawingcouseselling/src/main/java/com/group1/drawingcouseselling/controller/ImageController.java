d12de1f2b2816553d4ae353e233cb8dd0024e340
Anata Arisa <kakata28419@gmail.com>
2023-09-24 16:09:16 +0700

    thang/add image api

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/controller/ImageController.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/controller/ImageController.java
@@ -8,11 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class ImageController {
     private MetadataService metadataService;
