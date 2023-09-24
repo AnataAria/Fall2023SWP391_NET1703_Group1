90a89bf071770188ae143ae1539e043ea5148f38
Anata Arisa <kakata28419@gmail.com>
2023-09-24 15:56:56 +0700

    vinh/add config for oauth2

---
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/config/Oauth2SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.group1.drawingcouseselling.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@Order(2)
+public class Oauth2SecurityConfig {
+}
