f5fb669a891e74cc699f1ef23d0e06a32798405d
Anata Arisa <kakata28419@gmail.com>
2023-09-24 15:58:53 +0700

    vinh/add some method for query

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/repository/CustomerRepository.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/repository/CustomerRepository.java
@@ -2,10 +2,14 @@ package com.group1.drawingcouseselling.repository;
 
 import com.group1.drawingcouseselling.model.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, BigDecimal> {
+    @Query("SELECT c FROM customer c INNER JOIN account a ON c.account.id = a.id WHERE a.email = :email")
+    public Customer searchCustomerByAccountEmail(@Param(value = "email") String email);
 }
