4300e486bf1e780502458d060d0d8e05c35552cb
Anata Arisa <kakata28419@gmail.com>
2023-09-25 21:30:57 +0700

    vinh/add oauth2 and register

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/model/dto/RegisterRequest.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/model/dto/RegisterRequest.java
@@ -1,11 +1,19 @@
 package com.group1.drawingcouseselling.model.dto;
 
+import com.group1.drawingcouseselling.model.enums.EGender;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-public class RegisterRequest {
-    private String email;
-    private String password;
+import java.sql.Date;
+
+@Builder
+public record RegisterRequest(String email,String fullname, String password, Date birthDate, EGender gender) {
+    public RegisterRequest(String email,String fullname, String password, Date birthDate, EGender gender){
+        this.email = email;
+        this.fullname = fullname;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.gender = gender;
+    }
 }
