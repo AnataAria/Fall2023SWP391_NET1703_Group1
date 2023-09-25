4868af8d691439a1e3fb4d7bbc33096c82dab4f9
Anata Arisa <kakata28419@gmail.com>
2023-09-25 08:19:19 +0700

    vinh/add cart service

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/CartService.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/CartService.java
@@ -1,4 +1,9 @@
 package com.group1.drawingcouseselling.service;
 
+import com.group1.drawingcouseselling.model.dto.CartDto;
+
 public interface CartService {
+    public CartDto getAllCartOnPaging(Integer page, Integer maxPage, String email);
+    public void addCart(String email, Integer courseID);
+    public void removeCart(String email, Integer courseID);
 }
--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/impl/CartServiceImpl.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/impl/CartServiceImpl.java
@@ -1,26 +1,32 @@
 package com.group1.drawingcouseselling.service.impl;
 
 import com.group1.drawingcouseselling.exception.CourseMissMatchException;
+import com.group1.drawingcouseselling.model.dto.CartDto;
+import com.group1.drawingcouseselling.model.dto.CourseDto;
+import com.group1.drawingcouseselling.model.entity.Course;
 import com.group1.drawingcouseselling.repository.CartRepository;
+import com.group1.drawingcouseselling.repository.CourseRepository;
 import com.group1.drawingcouseselling.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class CartServiceImpl implements CartService {
     private final CartRepository cartRepository;
+    private final CourseRepository courseRepository;
 
     @Autowired
-    public CartServiceImpl(CartRepository cartRepository) {
+    public CartServiceImpl(CartRepository cartRepository, CourseRepository courseRepository) {
         this.cartRepository = cartRepository;
+        this.courseRepository = courseRepository;
     }
-
+    @Override
     public void addCart(String email, Integer courseID){
         String cartCookie = cartRepository.searchCartByAccountEmail(email);
         if(cartCookie != null) {
@@ -30,10 +36,10 @@ public class CartServiceImpl implements CartService {
             cartRepository.updateCart(email, cartCookie);
         }
     }
-
+    @Override
     public void removeCart(String email, Integer courseID){
         Collection<BigDecimal> cartList = convertStringToArray(email);
-        if(cartList.isEmpty()) {
+        if(!cartList.isEmpty()) {
             if(!cartList.add(BigDecimal.valueOf(courseID))) throw new CourseMissMatchException("");
             String cartCookie = convertSetToString(cartList);
             cartRepository.updateCart(email, cartCookie);
@@ -48,5 +54,20 @@ public class CartServiceImpl implements CartService {
 
     private String convertSetToString(Collection<BigDecimal> list){
         return String.join(",", list.stream().map(BigDecimal::toString).toList());
+    }
+
+    @Override
+    public CartDto getAllCartOnPaging(Integer page, Integer maxPage, String email) {
+        Collection<BigDecimal> cartList = convertStringToArray(email);
+        List<CourseDto> courseList = null;
+        if(!cartList.isEmpty()){
+            courseList = courseRepository.searchCourseByIdList(cartList).stream().map(a -> new Course().convertEntityToDto(a)).toList();
+            Pageable pageable = PageRequest.of(page, maxPage);
+            int start = (int) pageable.getOffset();
+            int end = Math.min((start + pageable.getPageSize()), courseList.size());
+            courseList  = courseList.subList(start,end);
+        }
+        if(courseList == null) courseList = new ArrayList<>();
+        return CartDto.builder().courseList(courseList).build();
     }
 }
