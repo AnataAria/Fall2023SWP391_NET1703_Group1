73d6e735a1847f69cf0b42a9bb666f8e73e0c323
Anata Arisa <kakata28419@gmail.com>
2023-09-27 13:16:12 +0700

    vinh/fix auth

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/controller/AuthenticationController.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.group1.drawingcouseselling.model.dto.AuthenticationRequest;
 import com.group1.drawingcouseselling.model.dto.RegisterRequest;
 import com.group1.drawingcouseselling.service.AuthenticationService;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,18 +16,20 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     @PostMapping("/register")
-    public ResponseEntity<Cookie> register (@RequestBody RegisterRequest request){
+    public ResponseEntity<Cookie> register (@RequestBody RegisterRequest request, HttpServletResponse response){
         Cookie cookie = new Cookie("USER",authenticationService.register(request).getToken());
         cookie.setMaxAge(24*64*64);
-        cookie.setDomain("localhost:9090");
+        cookie.setPath("/");
+        response.addCookie(cookie);
         return ResponseEntity.ok(cookie);
     }
 
     @PostMapping("/authentication")
-    public ResponseEntity<Cookie> authentication (@RequestBody AuthenticationRequest request){
+    public ResponseEntity<Cookie> authentication (@RequestBody AuthenticationRequest request, HttpServletResponse response){
         Cookie cookie = new Cookie("USER",authenticationService.authenticate(request).getToken());
         cookie.setMaxAge(24*60*60);
-        cookie.setDomain("localhost:9090");
+        cookie.setPath("/");
+        response.addCookie(cookie);
         return ResponseEntity.ok(cookie);
     }
 
