da87d2806c272bd78e00ef9e01fd895928440bb8
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2023-09-29 01:55:38 +0700

    Add OTP page

--- frontend/my-app/src/routes/otp/+page.svelte
+++ frontend/my-app/src/routes/otp/+page.svelte
@@ -2,7 +2,58 @@
   .form-gap {
     padding-top: 70px;
 }
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
 </style>
+
+<script lang="ts">
+    import axios, { AxiosError, type AxiosResponse } from "axios";
+    import { apiBaseUrl } from "../../service";
+    import { BarLoader, Circle, Circle2, Circle3, Jumper } from 'svelte-loading-spinners';
+  let otpnum:number
+  let message:string
+  async function validateOtp() {
+    disableSubmitButton();
+    let res;
+    res = await axios.get(apiBaseUrl +"auth/validateOtp", {
+      params: {
+        otpnum
+      }
+    })
+    .then((response : AxiosResponse) =>{        
+      enableSubmitButton();
+      if (response.status === 200){
+        window.location.href = '/changepassword'
+      }
+    })
+    .catch((reason : AxiosError) => {
+      enableSubmitButton();
+      if (reason.response?.status === 401){
+        message = "Invalid OTP, please try again"
+      }
+    })
+  } 
+
+  function disableSubmitButton() {
+        (document.getElementById('submitButton') as HTMLButtonElement).disabled = true;
+        (document.getElementById('loader') as HTMLElement).hidden = false;
+        (document.getElementById('resetpassword') as HTMLElement).hidden = true;
+
+    }
+    function enableSubmitButton(){
+        (document.getElementById('submitButton') as HTMLButtonElement).disabled = false;
+        (document.getElementById('loader') as HTMLElement).hidden = true;
+        (document.getElementById('resetpassword') as HTMLElement).hidden = false;
+    }
+</script>
 
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -19,16 +70,24 @@
                   <p>Please check your (spam) inbox for the OTP.</p>
                   <div class="panel-body">
     
-                    <form id="register-form" role="form" autocomplete="off" class="form" method="post">
+                    <form id="register-form" role="form" autocomplete="off" class="form">
     
                       <div class="form-group">
                         <div class="input-group">
                           <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
-                          <input id="email" name="email" placeholder="Enter OTP" class="form-control"  type="email">
+                          <input id="otp" name="otp" placeholder="Enter OTP" class="form-control"  type="number" bind:value={otpnum}>
                         </div>
                       </div>
+                      {#if message != null}
                       <div class="form-group">
-                        <input name="recover-submit" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
+                        <b style="color: red;">{message}</b>
+                      </div>
+                      {/if}
+                      <div class="form-group">
+                        <button name="recover-submit" id="submitButton" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit" on:click={validateOtp}>
+                          <span style="margin: 0 auto; display: table;" id="loader" hidden><Circle size="30" color="#ffffff" unit="px" duration="1s"/></span>
+                          <span id="resetpassword">Verify</span>
+                        </button>
                       </div>
                       
                       <input type="hidden" class="hide" name="token" id="token" value=""> 
