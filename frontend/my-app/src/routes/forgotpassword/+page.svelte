7e47f5e6abf7b2c68958445794b0ca741cb6bbae
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2023-09-29 01:56:43 +0700

    Add forgot password page

--- frontend/my-app/src/routes/forgotpassword/+page.svelte
+++ frontend/my-app/src/routes/forgotpassword/+page.svelte
@@ -1,48 +1,5 @@
-<style>
-  .form-gap {
-    padding-top: 70px;
-}
-</style>
+<script>
+    import ForgotPassword from "../ForgotPassword.svelte";
 
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-<div class="form-gap"></div>
-<div class="container">
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4">
-            <div class="panel panel-default">
-              <div class="panel-body">
-                <div class="text-center">
-                  <h3><i class="fa fa-lock fa-4x"></i></h3>
-                  <h2 class="text-center">Forgot Password?</h2>
-                  <p>You can reset your password here.</p>
-                  <div class="panel-body">
-    
-                    <form id="register-form" role="form" autocomplete="off" class="form" method="post">
-    
-                      <div class="form-group">
-                        <div class="input-group">
-                          <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
-                          <input id="email" name="email" placeholder="Email address" class="form-control"  type="email">
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <input name="recover-submit" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
-                      </div>
-                      <div class="form-group">
-                        <form action="/login">
-                          <input name="recover-submit" class="btn btn-lg btn-primary btn-block" value="Back To Login" type="submit">
-                        </form>
-                      </div>
-                      
-                      <input type="hidden" class="hide" name="token" id="token" value=""> 
-                    </form>
-    
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-	</div>
-</div>
+</script>
+<ForgotPassword/>
