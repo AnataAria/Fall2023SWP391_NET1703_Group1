162d1ad435af593c7c9caa1def6e5db9a2af8d53
thanlong421999 <thanlong421999@gmail.com>
2023-09-29 21:23:48 +0700

    ha/fix banner, search bar, header

--- frontend/my-app/src/routes/SearchBar.svelte
+++ frontend/my-app/src/routes/SearchBar.svelte
@@ -1,179 +1,119 @@
-<main>
-	<div class="container" style="padding-top: 1.1rem;">
-		
-		<div class="search-box">
-			<div class="search-icon"><i class="fa fa-search search-icon"></i></div>
-			<form action="" class="search-form">
-				<input type="text" placeholder="Search" id="search" autocomplete="off">
-			</form>
-			<svg class="search-border" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/" x="0px" y="0px" viewBox="0 0 671 111" style="enable-background:new 0 0 671 111;"
-			 xml:space="preserve">
-          <path class="border" d="M335.5,108.5h-280c-29.3,0-53-23.7-53-53v0c0-29.3,23.7-53,53-53h280"/>
-          <path class="border" d="M335.5,108.5h280c29.3,0,53-23.7,53-53v0c0-29.3-23.7-53-53-53h-280"/>
-        </svg>
-			<div class="go-icon"><i class="fa fa-arrow-right"></i></div>
-		</div>
-
-	</div>
-</main>
+<form class="form-wrapper cf" >
+  <input type="text" placeholder="Search here..." required>
+  <button type="submit">Search</button>
+</form> 
 
 <style>
+  .cf:before, .cf:after{
+    content:"";
+    display:table;
+}
  
-
-main {
-  position: absolute;
-  top: 0;
-  left: 0;
-  margin: 0 auto;
-  padding: 0 26px;
-  width: 100%;
-  height: 6rem;
-  position: relative;
-  background: rgb(160, 207, 97);
-
+.cf:after{
+    clear:both;
 }
-h1 {
-  display: block;
-  margin: 0 auto 25px auto;
-  text-align: center;
-  font-size: 1.92em;
-  font-weight: 600;
-  letter-spacing: -0.055em;
+ 
+.cf{
+    zoom:1;
+} 
+/* Form wrapper styling */
+.form-wrapper {
+    width: 480px;
+    padding: 15px;
+    margin-right: 400px;
+    background: #444;
+    background: rgba(0,0,0,0);
+    border: none;
+    /* box-shadow: 0 1px 1px rgba(0,0,0,.4) inset, 0 1px 0 rgba(255,255,255,.2); */
 }
-h2 {
-  display: block;
-  margin: 0 auto 60px auto;
-  text-align: center;
-  font-weight: 400;
-  font-size: 1.25em;
-  letter-spacing: -0.015em;
+ 
+/* Form text input */
+ 
+.form-wrapper input {
+    width: 330px;
+    height: 40px;
+    padding: 10px 5px;
+    float: left;    
+    font: bold 15px 'lucida sans', 'trebuchet MS', 'Tahoma';
+    border: 0;
+    background: #eee;
+    border-radius: 3px 0 0 3px;      
 }
-.container {
-  position: relative;
-  margin: calc(75px + 2vh + 2vw) auto 0 auto;
-  padding: 0;
-  width: 100%;
-  max-width: 840px;
+ 
+.form-wrapper input:focus {
+    outline: 0;
+    background: #fff;
+    box-shadow: 0 0 2px rgba(0,0,0,.8) inset;
 }
-.search-box {
-  position: relative;
-  width: 100%;
-  max-width: 360px;
-  height: 60px;
-  border-radius: 120px;
-  margin: 0 auto;
+ 
+.form-wrapper input::-webkit-input-placeholder {
+   color: #999;
+   font-weight: normal;
+   font-style: italic;
 }
-.search-icon, .go-icon {
-  position: absolute;
-  top: 0;
-  height: 60px;
-  width: 86px;
-  line-height: 61px;
-  text-align: center;
+ 
+.form-wrapper input:-moz-placeholder {
+    color: #999;
+    font-weight: normal;
+    font-style: italic;
 }
-.search-icon {
-  left: 0;
-  pointer-events: none;
-  font-size: 1.22em;
-  will-change: transform;
-  transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
-  -moz-transform: rotate(-45deg);
-  -o-transform: rotate(-45deg);
-  transform-origin: center center;
-  -webkit-transform-origin: center center;
-  -moz-transform-origin: center center;
-  -o-transform-origin: center center;
-  transition: transform 400ms 220ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-  -webkit-transition: transform 400ms 220ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-  -moz-transition: transform 400ms 220ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-  -o-transition: transform 400ms 220ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
+ 
+.form-wrapper input:-ms-input-placeholder {
+    color: #999;
+    font-weight: normal;
+    font-style: italic;
+}    
+ 
+/* Form submit button */
+.form-wrapper button {
+    overflow: visible;
+    position: relative;
+    float: right;
+    border: 0;
+    padding: 0;
+    cursor: pointer;
+    height: 40px;
+    width: 110px;
+    font: bold 15px/40px 'lucida sans', 'trebuchet MS', 'Tahoma';
+    color: #fff;
+    text-transform: uppercase;
+    background: red;
+    border-radius: 0 3px 3px 0;      
+    text-shadow: 0 -1px 0 rgba(0, 0 ,0, .3);
+}   
+   
+.form-wrapper button:hover{     
+    background: #1fbad6;
+    opacity:.9
+}   
+   
+.form-wrapper button:active,
+.form-wrapper button:focus{   
+    background: #1fbad6;
+    outline: 0;   
 }
-.si-rotate {
-  transform: rotate(0deg);
-  -webkit-transform: rotate(0deg);
-  -moz-transform: rotate(0deg);
-  -o-transform: rotate(0deg);
+ 
+.form-wrapper button:before { /* left arrow */
+    content: '';
+    position: absolute;
+    border-width: 8px 8px 8px 0;
+    border-style: solid solid solid none;
+    border-color: transparent #1fbad6 transparent;
+    top: 12px;
+    left: -6px;
 }
-.go-icon {
-  right: 0;
-  pointer-events: none;
-  font-size: 1.38em;
-  will-change: opacity;
-  cursor: default;
-  opacity: 0;
-  transform: rotate(45deg);
-  -webkit-transform: rotate(45deg);
-  -moz-transform: rotate(45deg);
-  -o-transform: rotate(45deg);
-  transition: opacity 190ms ease-out, transform 260ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-  -webkit-transition: opacity 190ms ease-out, transform 260ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-  -moz-transition: opacity 190ms ease-out, transform 260ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-  -o-transition: opacity 190ms ease-out, transform 260ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
+ 
+.form-wrapper button:hover:before{
+    border-right-color: #1fbad6;
 }
-.go-in {
-  opacity: 1;
-  pointer-events: all;
-  cursor: pointer;
-  transform: rotate(0);
-  -webkit-transform: rotate(0);
-  -moz-transform: rotate(0);
-  -o-transform: rotate(0);
-  transition: opacity 190ms ease-out, transform 260ms 20ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-  -webkit-transition: opacity 190ms ease-out, transform 260ms 20ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-  -moz-transition: opacity 190ms ease-out, transform 260ms 20ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-  -o-transition: opacity 190ms ease-out, transform 260ms 20ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
-}
-.search-border {
-  display: block;
-  width: 100%;
-  max-width: 360px;
-  height: 60px;
-}
-.border {
-  fill: none;
-  stroke: #FFFFFF;
-  stroke-width: 5;
-  stroke-miterlimit: 10;
-}
-.border {
-  stroke-dasharray: 740;
-  stroke-dashoffset: 0;
-  transition: stroke-dashoffset 400ms cubic-bezier(0.600, 0.040, 0.735, 0.990);
-  -webkit-transition: stroke-dashoffset 400ms cubic-bezier(0.600, 0.040, 0.735, 0.990);
-  -moz-transition: stroke-dashoffset 400ms cubic-bezier(0.600, 0.040, 0.735, 0.990);
-  -o-transition: stroke-dashoffset 400ms cubic-bezier(0.600, 0.040, 0.735, 0.990);
-}
-.border-searching .border {
-  stroke-dasharray: 740;
-  stroke-dashoffset: 459;
-  transition: stroke-dashoffset 650ms cubic-bezier(0.755, 0.150, 0.205, 1.000);
-  -webkit-transition: stroke-dashoffset 650ms cubic-bezier(0.755, 0.150, 0.205, 1.000);
-  -moz-transition: stroke-dashoffset 650ms cubic-bezier(0.755, 0.150, 0.205, 1.000);
-  -o-transition: stroke-dashoffset 650ms cubic-bezier(0.755, 0.150, 0.205, 1.000);
-}
-#search {
-  font-family: 'Montserrat Alternates', sans-serif;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 120px;
-  border: none;
-  background: rgba(255,255,255,0);
-  padding: 0 68px 0 68px;
-  color: #FFFFFF;
-  font-size: 1.32em;
-  font-weight: 400;
-  letter-spacing: -0.015em;
-  outline: none;
-}
-#search::-webkit-input-placeholder {color: #FFFFFF;}
-#search::-moz-placeholder {color: #FFFFFF;}
-#search:-ms-input-placeholder {color: #FFFFFF;}
-#search:-moz-placeholder {color: #FFFFFF;}
-#search::-moz-selection {color: #FFFFFF; background: rgba(0,0,0,0.25);}
-#search::selection {color: #FFFFFF; background: rgba(0,0,0,0.25);}
-
+ 
+.form-wrapper button:focus:before,
+.form-wrapper button:active:before{
+        border-right-color: #1fbad6;
+}      
+ 
+.form-wrapper button::-moz-focus-inner { /* remove extra button spacing for Mozilla Firefox */
+    border: 0;
+    padding: 0;
+}    
 </style>
