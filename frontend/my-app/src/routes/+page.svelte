43c5369d1acb77938812d8beb08e085868f56cb8
Anata Arisa <kakata28419@gmail.com>
2023-09-29 18:42:23 +0700

    vinh/testing

--- frontend/my-app/src/routes/+page.svelte
+++ frontend/my-app/src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
     import { onMount } from 'svelte';
     import Top from './Top.svelte';
-    
-    
+    export const ssr = false;
+    import { Router, Route } from 'svelte-routing';
     import CourseCard from './CourseCard.svelte';
     import Star from './Star.svelte';
     import SearchBar from './SearchBar.svelte';
@@ -11,6 +11,7 @@
     import Header from './Header.svelte';
     import Banner from './Banner.svelte';
   import Carousel from './Carousel.svelte';
+  import Course from './course/+page.svelte'
   
     // Add this to initialize the Bootstrap tooltip
     onMount(() => {
@@ -19,6 +20,8 @@
   </script>
   
   
-  <Carousel />
+  <Header></Header>
+  <SearchBar/>
+  <!-- <Carousel></Carousel> -->
   
 
