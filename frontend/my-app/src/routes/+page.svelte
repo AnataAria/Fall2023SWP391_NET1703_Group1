b94522be9df2367464f45348e079480b7168c605
thanlong421999 <thanlong421999@gmail.com>
2023-10-02 16:28:31 +0700

    ha/add CustomerProfile

--- frontend/my-app/src/routes/+page.svelte
+++ frontend/my-app/src/routes/+page.svelte
@@ -1,21 +1,18 @@
-<script>
+<script lang="ts">
     import { onMount } from 'svelte';  
     import SearchBar from './SearchBar.svelte';
     import Header from './Header.svelte';
     import Banner from './Banner.svelte';
   import Carousel from './Carousel.svelte';
+  import CustomerProfile from './CustomerProfile.svelte';
+  
+  import ChangePassword from './ChangePassword.svelte';
   
     // Add this to initialize the Bootstrap tooltip
     onMount(() => {
     });
   </script>
-  <Header />
-  <SearchBar />
-  <Banner />
-  <br/>
-  <br/>
-  <Carousel />
-
+  <CustomerProfile />
   
   
 
