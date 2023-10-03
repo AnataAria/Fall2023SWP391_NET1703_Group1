bbfa70c308fab75abeb94f78b1111fce6210d635
thanlong421999 <thanlong421999@gmail.com>
2023-10-03 21:26:52 +0700

    ha/add cart, cartitem.totalcart

---
+++ frontend/my-app/src/routes/TotalCart.svelte
@@ -0,0 +1,21 @@
+<div class="bg-white rounded-lg shadow-md p-6">
+    <h2 class="text-lg font-semibold mb-4">Summary</h2>
+    <div class="flex justify-between mb-2">
+        <span>Subtotal</span>
+        <span>$19.99</span>
+    </div>
+    <div class="flex justify-between mb-2">
+        <span>Taxes</span>
+        <span>$1.99</span>
+    </div>
+    <div class="flex justify-between mb-2">
+        <span>Shipping</span>
+        <span>$0.00</span>
+    </div>
+    <hr class="my-2">
+    <div class="flex justify-between mb-2">
+        <span class="font-semibold">Total</span>
+        <span class="font-semibold">$21.98</span>
+    </div>
+    <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+</div>
