9ede8fcd0b5797c971a9df12c58297ad5f5b84d6
Anata Arisa <kakata28419@gmail.com>
2023-10-03 11:29:09 +0700

    vinh/remove unuse import

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/controller/AccountController.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/controller/AccountController.java
@@ -1,13 +1,11 @@
 package com.group1.drawingcouseselling.controller;
 
 import com.group1.drawingcouseselling.model.dto.AccountDto;
-import com.group1.drawingcouseselling.model.entity.Account;
 import com.group1.drawingcouseselling.model.enums.ERole;
 import com.group1.drawingcouseselling.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
