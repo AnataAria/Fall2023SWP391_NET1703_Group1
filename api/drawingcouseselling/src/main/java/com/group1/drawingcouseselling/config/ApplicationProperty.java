780198fef03ce96325ce00eff70d6d8e2908269b
Anata Arisa <kakata28419@gmail.com>
2023-10-05 09:20:12 +0700

    vinh/fix @Value bug null

---
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/config/ApplicationProperty.java
@@ -0,0 +1,9 @@
+package com.group1.drawingcouseselling.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationProperty {
+    public final String API_BASE_URL = "http://localhost:9090/api/v1/";
+    public final String FRONTEND_URL = "http://localhost:3000/";
+}
