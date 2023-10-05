aa2f8044f269a294fa1f9e1b92ffcf75c581e6ce
Anata Arisa <kakata28419@gmail.com>
2023-10-05 18:39:47 +0700

    vinh/fix bug on totalcart and redirect to pay method

--- frontend/my-app/src/routes/TotalCart.svelte
+++ frontend/my-app/src/routes/TotalCart.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
     export let sub:number;
     export let taxes:number;
-    export let shipping: number;
     export let total:number;
+    import {Button} from "flowbite-svelte";
+    function checkOut(){
+        window.location.href="/pay"
+    }
+    import {CurrencyHandler} from "../service";
 </script>
 
 <div class="bg-white rounded-lg shadow-md p-6">
     <h2 class="text-lg font-semibold mb-4">Summary</h2>
     <div class="flex justify-between mb-2">
         <span>Subtotal</span>
-        <span>${sub}</span>
+        <span>${CurrencyHandler(sub)}</span>
     </div>
     <div class="flex justify-between mb-2">
         <span>Taxes</span>
-        <span>${taxes}</span>
-    </div>
-    <div class="flex justify-between mb-2">
-        <span>Shipping</span>
-        <span>${shipping}</span>
+        <span>${CurrencyHandler(taxes)}</span>
     </div>
     <hr class="my-2">
     <div class="flex justify-between mb-2">
         <span class="font-semibold">Total</span>
-        <span class="font-semibold">${total}</span>
+        <span class="font-semibold">${CurrencyHandler(total)}</span>
     </div>
-    <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+    <Button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full" on:click={()=>{
+        checkOut();
+    }}>Check out</Button>
 </div>
