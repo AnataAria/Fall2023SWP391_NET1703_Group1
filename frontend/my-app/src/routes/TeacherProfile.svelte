589b8b7d8e7e3096ef8fbc1c11d1e9bf85cff7ff
hoangbaohuy <hbhuy2001119714@gmail.com>
2023-10-05 13:53:01 +0700

    huy/add instructor profile

---
+++ frontend/my-app/src/routes/TeacherProfile.svelte
@@ -0,0 +1,197 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
+<div class="bg-gray-100 pt-20">
+	
+    <div class="container mx-auto py-8">
+        <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
+			<!--Left-->
+            <div class="col-span-4 sm:col-span-3">
+                <div class="bg-white shadow rounded-lg p-6">
+                    <div class="flex flex-col ">
+                        <div class = "items-center">
+                          <img class = "mx-auto w-2/5 rounded-lg" src ="https://scontent.xx.fbcdn.net/v/t1.15752-9/384527701_207166732257295_191374629976122951_n.jpg?stp=dst-jpg_s206x206&_nc_cat=109&ccb=1-7&_nc_sid=aee45a&_nc_ohc=vAuht4IB9AgAX8LtnXT&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdTaA-FOdDgEHBlJNHUe3qr7Kqe3pd6wM66h30NWjjGCUA&oe=65447FAE" alt="">
+                        </div>           
+                        <div class = "font-bold text-xl text-center">Hoang Bao Huy</div>
+                        <br>
+                        <div>
+                            <i class="fa-solid fa-briefcase"></i>
+                            FPT University Student
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-location-dot"></i>
+                            Lam Dong, Viet Nam
+                        </div>
+                        <br>
+                        <div class = "text-lg text-gray-500">Email address</div>
+                        <div class = "font-medium">hbhuyademy@gmail.com</div>
+                        <br>                      
+                        <div class = "text-lg text-gray-500">Phone number</div>
+                        <div class = "font-medium">+84 933 565 468</div>
+                    </div>
+                    <hr class="my-6 border-t border-gray-300">
+                    <div class="flex flex-col">
+                        <span class="text-black font-bold tracking-wider mb-2 text-xl font-bold">Specialization</span>						
+                        <ul>
+                            <div class ="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">JavaScript</div>
+                            <div class ="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">React</div>
+                            <div class ="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">NodeJS</div>
+                            <div class ="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">HTML/ CSS</div>
+                            <div class ="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">Tailwind</div>
+                        </ul>
+                    </div>
+                    <hr class="my-6 border-t border-gray-300">
+					<div class="flex flex-col">
+                        <span class="text-black font-bold tracking-wider mb-2 text-xl font-bold">Hobbies</span>
+                        <ul>
+							<div class = "text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">Cats</div>
+							<div class = "text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">Animes</div>
+							<div class = "text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">Gaming</div>
+							<div class = "text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">Movies</div>
+							<div class = "text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-block">Travelling</div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+			<!--Right-->
+            <div class="col-span-4 sm:col-span-9">
+                <div class="bg-white shadow rounded-lg p-6">
+                    <h1 class="text-2xl font-bold mb-4">General information</h1>
+                    <h2 class="text-xl font-medium mb-4">About Me</h2>
+                    <p class="text-gray-700">
+                        Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.
+                    </p>
+                    <br>
+                    <p class="text-gray-700">
+                        Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.
+                    </p>
+                    <br>
+                    <br>
+                    <!--1-->
+					<div class="grid grid-cols-2 gap-4">										
+						<div>
+						  <div>Education</div>
+						  <div class="font-medium">FPT University</div>
+						</div>
+						<div>
+						  <div>Join Date</div>
+						  <div class="font-medium">12-09-2021</div>
+						</div>
+						<div>
+						  <div>Organization</div>
+						  <div class="font-medium">FPT</div>
+						</div>
+						<div>
+						  <div>Department</div>
+						  <div class="font-medium">Drawing</div>
+						</div>				
+						<div>
+						  <div>Work History</div>
+						  <div class="font-medium">Facebook, Google, Discord</div>
+						</div>
+						<div>
+						  <div>Languages</div>
+						  <div class="font-medium">English, Japan, Vietnam</div>
+						</div>
+						<div>
+						  <div>Role</div>
+						  <div class="font-medium">Instructor</div>
+						</div>	
+						<div>
+						  <div>Birthday</div>
+						  <div class="font-medium">01-12-2003</div>
+						</div>										
+					</div>       
+                    <hr class="my-6 border-t border-gray-300">     
+                    <!--2-->
+                    <div class="grid grid-cols-2 gap-4">										
+						<div>
+						  <div>Caricature drawing</div>
+						  <div class="relative pt-1">                          
+                            <div class="flex flex-col">
+                              <div class="w-full bg-gray-200 rounded-full">
+                                <div class="w-2/3 bg-teal-600 py-1 rounded-full"></div>
+                              </div>
+                            </div>
+                          </div>	
+						</div>
+						<div>
+						  <div>Cartoon drawing</div>
+						  <div class="relative pt-1">                          
+                            <div class="flex flex-col">
+                              <div class="w-full bg-gray-200 rounded-full">
+                                <div class="w-1/3 bg-teal-600 py-1 rounded-full"></div>
+                              </div>
+                            </div>
+                          </div>	
+						</div>
+						<div>
+						  <div>Pointillism</div>
+						  <div class="relative pt-1">                          
+                            <div class="flex flex-col">
+                              <div class="w-full bg-gray-200 rounded-full">
+                                <div class="w-3/4 bg-teal-600 py-1 rounded-full"></div>
+                              </div>
+                            </div>
+                          </div>	
+						</div>
+						<div>
+						  <div>Figure drawing</div>
+						  <div class="relative pt-1">                          
+                            <div class="flex flex-col">
+                              <div class="w-full bg-gray-200 rounded-full">
+                                <div class="w-4/5 bg-teal-600 py-1 rounded-full"></div>
+                              </div>
+                            </div>
+                          </div>	
+						</div>				
+						<div>
+                            <div>
+                              Gesture drawing
+                            </div>
+                            <div class="relative pt-1">                          
+                                <div class="flex flex-col">
+                                  <div class="w-full bg-gray-200 rounded-full">
+                                    <div class="w-5/6 bg-teal-600 py-1 rounded-full"></div>
+                                  </div>
+                                </div>
+                            </div>	                    
+                        </div>
+						<div>
+						  <div>Line drawing</div>
+						  <div class="relative pt-1">                          
+                            <div class="flex flex-col">
+                              <div class="w-full bg-gray-200 rounded-full">
+                                <div class="w-3/4 bg-teal-600 py-1 rounded-full"></div>
+                              </div>
+                            </div>
+                          </div>	
+						</div>
+						<div>
+						  <div>Perspective drawing</div>
+						  <div class="relative pt-1">                          
+                            <div class="flex flex-col">
+                              <div class="w-full bg-gray-200 rounded-full">
+                                <div class="w-3/5 bg-teal-600 py-1 rounded-full"></div>
+                              </div>
+                            </div>
+                          </div>	
+						</div>	
+						<div>
+						  <div>Photorealism</div>
+						  <div class="relative pt-1">                          
+                            <div class="flex flex-col">
+                              <div class="w-full bg-gray-200 rounded-full">
+                                <div class="w-1/2 bg-teal-600 py-1 rounded-full"></div>
+                              </div>
+                            </div>
+                          </div>	
+						</div>	                       
+					</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+  
+
+  
