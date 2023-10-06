d4007607c599436d9fbe4ead7cadbcd00fad0280
Anata Arisa <kakata28419@gmail.com>
2023-10-06 23:00:41 +0700

    vinh/add banner and course list

--- frontend/my-app/src/routes/+page.svelte
+++ frontend/my-app/src/routes/+page.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import "../app.postcss";
-  
-  import Banner from "./Banner.svelte";
-  import CourseDetail from "./CourseDetail.svelte";
+  export const images = [
+    {
+      alt: 'Cosmic timetraveler',
+      src: 'https://static.miraheze.org/bluearchivewiki/0/0f/Arisu.png?version=8fe2ae44d97dabab9a4d147a3bbd158c',
+      title: 'cosmic-timetraveler-pYyOZ8q7AII-unsplash.com'
+    },
+    {
+      alt: 'Cristina Gottardi',
+      src: 'https://static.miraheze.org/bluearchivewiki/0/0f/Arisu.png?version=8fe2ae44d97dabab9a4d147a3bbd158c',
+      title: 'cristina-gottardi-CSpjU6hYo_0-unsplash.com'
+    }
+  ];
   import Carousel from "./Carousel.svelte";
-  
+  import CourseCard from "./CourseCard.svelte";
 </script>
-<Carousel></Carousel>
+<Carousel/>
+<div class="">
 
-<CourseDetail></CourseDetail>
+</div>
+<div class="flex flex-nowrap space-x-8">
+  <CourseCard></CourseCard>
+  <CourseCard></CourseCard>
+  <CourseCard></CourseCard>
+  <CourseCard></CourseCard>
+</div>
