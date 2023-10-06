529232964cad237c8631a076a3bc4ddd28c615ee
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2023-10-06 22:33:57 +0700

    Change card scroll container

---
+++ frontend/my-app/src/routes/CardScrollContainer.svelte
@@ -0,0 +1,216 @@
+<script>
+  import CourseCard from "./CourseCard.svelte";
+</script>
+
+<div class="mt-32">
+  <div class="media-scroller snaps-inline">
+    <div class="media-element">
+      <CourseCard />
+      <p class="title">Short title</p>
+    </div>
+    <div class="media-element">
+      <CourseCard />
+      <p class="title">Short title</p>
+    </div>
+    <div class="media-element">
+      <CourseCard />
+      <p class="title">Short title</p>
+    </div>
+    <div class="media-element">
+      <CourseCard />
+      <p class="title">Short title</p>
+    </div>
+    <div class="media-element">
+      <CourseCard />
+      <p class="title">Short title</p>
+    </div>
+    <div class="media-element">
+      <CourseCard/>
+      <p class="title">Short title</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1642543492481-44e81e3914a7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc2Mw&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Duis aute irure dolor</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1641118961077-440391095cdc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc2Mw&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Cillum dolore eu</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1640767014413-b7d27c58b058?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc5NQ&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title" />
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1640948612546-3b9e29c23e98?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc5NQ&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Excepteur sint occaecat cupidatat non proident</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1642484865851-111e68695d71?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc5NQ&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">At lectus urnaVestibulum</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1642237778207-24985a0bf876?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc5NQ&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Vestibulum</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1642177584449-fa0b017dccc7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc5NQ&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Adipiscing tristique risus nec feugiat</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1643249960396-d39d2a63ce8a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODg0Mw&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Rutrum tellus pellentesque eu tincidunt</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1641424222187-1c336d21804c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODg0OA&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Bibendum enim</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1640998483268-d1faffa789ad?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODkwNA&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Amet commodo</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1642034451735-2a8df1eaa2c0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODg4OQ&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">A erat nam at lectus</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1640808238224-5520de93c939?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODg4OQ&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Pellentesque eu tincidunt tortor aliquam nulla</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1643039952431-38adfa91f320?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODg0OA&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Quam adipiscing vitae</p>
+    </div>
+    <div class="media-element">
+      <img
+        src="https://images.unsplash.com/photo-1643148636637-58b3eb95cdad?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODg0OA&ixlib=rb-1.2.1&q=80&w=400"
+        alt=""
+      />
+      <p class="title">Fermentum</p>
+    </div>
+  </div>
+</div>
+
+<style>
+  @import "https://unpkg.com/open-props";
+  @import "https://unpkg.com/open-props/normalize.min.css";
+
+  .media-scroller {
+    --_spacer: var(--size-3);
+    display: grid;
+    gap: var(--_spacer);
+    grid-auto-flow: column;
+    grid-auto-columns: 21%;
+
+    padding: 0 var(--_spacer) var(--_spacer);
+
+    overflow-x: auto;
+    overscroll-behavior-inline: contain;
+  }
+
+  .media-scroller--with-groups {
+    grid-auto-columns: 80%;
+  }
+
+  .media-group {
+    display: grid;
+    gap: var(--_spacer);
+    grid-auto-flow: column;
+  }
+
+  .media-element {
+    display: grid;
+    grid-template-rows: min-content;
+    gap: var(--_spacer);
+    padding: var(--_spacer);
+    /* background: var(--surface-2); */
+    border-radius: var(--radius-2);
+    box-shadow: var(--shadow-2);
+  }
+
+  .media-element > img {
+    inline-size: 100%;
+    aspect-ratio: 16 / 9;
+    object-fit: cover;
+  }
+
+  .snaps-inline {
+    scroll-snap-type: inline mandatory;
+    scroll-padding-inline: var(--_spacer, 1rem);
+  }
+
+  .snaps-inline > * {
+    scroll-snap-align: start;
+  }
+
+  /* general styling */
+
+  .container {
+    inline-size: min(100% - 4rem, 70rem);
+    margin-inline: auto;
+  }
+
+  .flow {
+    display: grid;
+    gap: var(--size-3);
+  }
+
+  .page-header {
+    padding-block: var(--size-9);
+    margin-block-end: var(--size-9);
+    background: var(--gradient-16);
+    color: var(--gray-0);
+    box-shadow: var(--shadow-2);
+  }
+
+  .page-title {
+    font-size: var(--font-size-fluid-3);
+  }
+
+  .page-subtitle {
+    font-size: var(--font-size-fluid-1);
+  }
+
+  .section-title {
+    padding-inline-start: var(--size-6);
+    margin-block: var(--size-9) var(--size-3);
+  }
+</style>
