ec6933c42ba132a8a871d7bc3bc6ee9cca44a3bd
Anata Arisa <kakata28419@gmail.com>
2023-10-09 18:18:19 +0700

    vinh/add method for checkout cart

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/CartService.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/CartService.java
@@ -1,12 +1,16 @@
 package com.group1.drawingcouseselling.service;
 
 import com.group1.drawingcouseselling.model.dto.CartDto;
+import com.group1.drawingcouseselling.model.entity.Course;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface CartService {
     public CartDto getAllCartOnPaging(Integer page, Integer maxPage, String email);
     public CartDto addCart(String email, Integer courseID);
     public void removeCart(String email, Integer courseID);
     public BigDecimal calculateBaseTotalCart(String email);
+    public List<Course> getAllCartForPayment(String email);
+    public void refreshCart(String email);
 }
