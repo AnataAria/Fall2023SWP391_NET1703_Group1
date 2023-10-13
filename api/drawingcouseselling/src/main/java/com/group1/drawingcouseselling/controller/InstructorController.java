6a4d2fee8c7da557e479874d44aabb13419e1411
Anata Arisa <kakata28419@gmail.com>
2023-10-14 04:12:49 +0700

    vinh/add instructor

---
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/controller/InstructorController.java
@@ -0,0 +1,17 @@
+package com.group1.drawingcouseselling.controller;
+
+import com.group1.drawingcouseselling.model.dto.InstructorDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+public class InstructorController {
+//    @GetMapping("")
+//    public ResponseEntity<InstructorDto> getInstructorInfo(@RequestHeader(required = false) String instructorToken
+//        ,@RequestParam(value = "instructorID", required = false) BigDecimal instructorID
+//    ){
+//    }
+}
