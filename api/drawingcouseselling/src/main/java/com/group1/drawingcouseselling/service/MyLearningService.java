119f5dd8a0873e95329a1acc1bd42d3da5b732c0
Anata Arisa <kakata28419@gmail.com>
2023-10-14 04:10:21 +0700

    vinh/add method for checking cart

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/MyLearningService.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/MyLearningService.java
@@ -2,9 +2,12 @@ package com.group1.drawingcouseselling.service;
 
 import com.group1.drawingcouseselling.model.dto.CourseDto;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface MyLearningService {
     public void orderCourse(String email);
     public List<CourseDto> getLearningCourseList(String email);
+
+    public boolean hasCourse(String email, BigDecimal courseID);
 }
