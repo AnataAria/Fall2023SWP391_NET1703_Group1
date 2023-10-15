d25b50a1c0b452fa87335dd5726b5e2f0577d748
Anata Arisa <kakata28419@gmail.com>
2023-10-16 02:31:45 +0700

    vinh/add search by instructor email

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/repository/InstructorRepository.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/repository/InstructorRepository.java
@@ -2,9 +2,15 @@ package com.group1.drawingcouseselling.repository;
 
 import com.group1.drawingcouseselling.model.entity.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.util.Optional;
+
 @Repository
 public interface InstructorRepository extends JpaRepository<Instructor, BigDecimal> {
+    @Query(value = "SELECT i FROM instructor i INNER JOIN account a ON a.id = i.account.id WHERE a.email = :instructorEmail")
+    public Optional<Instructor> findInstructorByEmail(@Param(value = "instructorEmail")String instructorEmail);
 }
