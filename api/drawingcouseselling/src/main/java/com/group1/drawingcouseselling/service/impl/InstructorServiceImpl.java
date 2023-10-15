2d7f98636c7e20a31202cba37063ae7d67a392ed
Anata Arisa <kakata28419@gmail.com>
2023-10-16 02:31:23 +0700

    vinh/update instructor service

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/impl/InstructorServiceImpl.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/service/impl/InstructorServiceImpl.java
@@ -1,6 +1,8 @@
 package com.group1.drawingcouseselling.service.impl;
 
+import com.group1.drawingcouseselling.exception.UserNotFoundException;
 import com.group1.drawingcouseselling.exception.ValueIsInvalidException;
+import com.group1.drawingcouseselling.model.dto.InstructorDto;
 import com.group1.drawingcouseselling.model.entity.Instructor;
 import com.group1.drawingcouseselling.repository.InstructorRepository;
 import com.group1.drawingcouseselling.service.InstructorService;
@@ -27,4 +29,9 @@ public class InstructorServiceImpl implements InstructorService {
         }
         return Optional.of(result);
     }
+    @Override
+    public Instructor findInstructorByInstructorEmail(String instructorEmail){
+        return instructorRepository.findInstructorByEmail(instructorEmail).orElseThrow(() -> new UserNotFoundException(""));
+    }
+
 }
