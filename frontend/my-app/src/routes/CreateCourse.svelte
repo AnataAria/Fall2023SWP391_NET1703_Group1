dc3c3e20fa31cfb75d2884acf4b9889233bd2af3
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2023-10-17 20:39:39 +0700

    Add validation

--- frontend/my-app/src/routes/CreateCourse.svelte
+++ frontend/my-app/src/routes/CreateCourse.svelte
@@ -12,31 +12,40 @@
     import { CheckCircleSolid, CloseCircleSolid } from "flowbite-svelte-icons";
     import { quintOut } from "svelte/easing";
     import axios, { Axios, AxiosError, type AxiosResponse } from "axios";
-    import { apiBaseUrl } from "../service";
+    import { GetCookie, apiBaseUrl } from "../service";
+    import type { CourseCreate } from "$lib/types";
     let open = true;
     let counter = 6;
     let message = "";
     let errorMsg = "";
 
     let formModal = false;
-    let Course = {
-        course_name: "",
+    let course:CourseCreate = {
+        name: "",
         description: "",
-        duration: "",
-        price: 0,
-    };
+        durations: "",
+        price: 0
+    }
     function CheckInput() {
-        console.log(Course);
-        if (Math.sign(Course.price) === -1 || Course.price === 0) {
+        console.log(course);
+        if (Math.sign(course.price) === -1 || course.price === 0) {
             showErrMessage("Price cannot be less than or equal to 0");
+        }
+        else{
+            CreateCourse();
         }
     }
     async function CreateCourse() {
         let res;
-        res = await axios.post(apiBaseUrl, Course)
+        res = await axios.post(apiBaseUrl + "course", course, {
+            headers: {
+                Authorization: `Bearer ${GetCookie("USER")}`
+            }
+        })
         .then((respone : AxiosResponse) => {
             if (respone.status === 200){
-                showMessage(Course.course_name + " created successfully!")
+                showMessage(course.name + " created successfully!")
+                window.location.href = "/profile/instructor"
             }
         })
         .catch ((error : AxiosError) => {
@@ -79,7 +88,7 @@
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
-                bind:value={Course.course_name}
+                bind:value={course.name}
             />
             <label
                 for="course_title"
@@ -98,7 +107,7 @@
                 rows="4"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Give some description about your course..."
-                bind:value={Course.description}
+                bind:value={course.description}
             />
         </div>
         <div class="relative z-0 w-full mb-6 group">
@@ -109,7 +118,7 @@
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
-                bind:value={Course.duration}
+                bind:value={course.durations}
             />
             <label
                 for="duration"
@@ -125,12 +134,12 @@
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
-                bind:value={Course.price}
+                bind:value={course.price}
             />
             <label
                 for="pricing"
                 class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Pricing</label
+                >Pricing (VND)</label
             >
         </div>
         <button
