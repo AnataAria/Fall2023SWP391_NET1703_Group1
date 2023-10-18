e87e2a1e63fcd7e437dcffad17b9560318dd8e05
Anata Arisa <kakata28419@gmail.com>
2023-10-18 17:00:22 +0700

    vinh/add sectiondto for create section

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/model/dto/SectionCreateDto.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/model/dto/SectionCreateDto.java
@@ -1,4 +1,17 @@
 package com.group1.drawingcouseselling.model.dto;
 
-public record SectionCreateDto() {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+@Builder
+public record SectionCreateDto(
+        @NotNull(message = "CourseID is mandatory")
+        @Min(value = 0, message = "CourseID is mandatory") BigDecimal courseID,
+        SectionDto sectionInfo) {
+    public SectionCreateDto(BigDecimal courseID, SectionDto sectionInfo){
+        this.courseID = courseID;
+        this.sectionInfo = sectionInfo;
+    }
 }
