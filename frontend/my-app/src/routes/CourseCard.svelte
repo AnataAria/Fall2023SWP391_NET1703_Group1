7e9508b7fb72500f25d881477d31f2e187e2affe
Anata Arisa <kakata28419@gmail.com>
2023-10-19 20:38:33 +0700

    vinh/fix merge conflict

--- frontend/my-app/src/routes/CourseCard.svelte
+++ frontend/my-app/src/routes/CourseCard.svelte
@@ -12,6 +12,37 @@
     export let instructorId: number;
     export let isFetchManual: boolean;
     import { Card, Button, Rating, Badge } from 'flowbite-svelte';
+    import { Toast } from "flowbite-svelte";
+    import { CheckCircleSolid, CloseCircleSolid } from "flowbite-svelte-icons";
+    import { quintOut } from "svelte/easing";
+    import { slide } from "svelte/transition";
+  import ToastShowMessage from "../templates/ToastShowMessage.svelte";
+    let show = false;
+    let open = true;
+    let counter = 6;
+    let message = "";
+    let errorMsg = "";
+    export function showMessage(value:string){
+      message = "";
+      errorMsg = "";
+      open = true;
+      counter = 6;
+      timeout();
+      message = value;
+      }
+     export function showErrMessage(value:string){
+      message = "";
+      errorMsg = "";
+      open = true;
+      counter = 6;
+      timeout();
+      errorMsg = value;
+    }
+    function timeout() {
+        if (--counter > 0) return setTimeout(timeout, 1000);
+        open = false;
+    }
+
     let jwtToken:string | null = "";
     let course = {
         name: "",
@@ -35,15 +66,15 @@
         })
         .then((response) => {
             if(response.status === 200){
-              alert("Add cart successfully")
+              showMessage("Add Cart Successfully")
             }
             if(response.status === 405){
-              alert("Already in cart");
+              showErrMessage("Add Cart Failed");
             }
          });
       }catch(error){
         console.log(error);
-        alert("Already in cart");
+        showErrMessage("Add Cart Failed");
       }
     }
     async function handleGetCourse() {
@@ -105,3 +136,28 @@
     </div>
   </div>
 </Card>
+
+<!-- {#if errorMsg != ""}
+   <div>
+    <Toast color="red" dismissable={false} bind:open transition={slide}>
+      <svelte:fragment slot="icon">
+        <CloseCircleSolid class="w-5 h-5" />
+        <span class="sr-only">Error icon</span>
+      </svelte:fragment>
+      {errorMsg}
+    </Toast>
+  </div>
+  {/if}
+  {#if message != ""}
+  <div>
+    <Toast color="green" transition={slide} params={{ delay: 250, duration: 300, easing: quintOut }} dismissable={false} bind:open>
+      <svelte:fragment slot="icon">
+        <CheckCircleSolid class="w-5 h-5" />
+        <span class="sr-only">Check icon</span>
+      </svelte:fragment>
+      {message}.
+    </Toast>
+    
+  </div>
+  {/if} -->
+  <ToastShowMessage></ToastShowMessage>
