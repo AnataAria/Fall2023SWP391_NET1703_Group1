f78c53463005221b66f4e4ed279cd203eea173db
Anata Arisa <kakata28419@gmail.com>
2023-10-23 02:07:48 +0700

    vinh/add CourseNotOwnedException

---
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/exception/CourseNotOwnedException.java
@@ -0,0 +1,16 @@
+package com.group1.drawingcouseselling.exception;
+
+import com.group1.drawingcouseselling.model.dto.ErrorMessage;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public class CourseNotOwnedException extends BaseException {
+    public CourseNotOwnedException(String message) {
+        super(message);
+        this.errorMessage = ErrorMessage.builder()
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .errorList(List.of(message.split(",")))
+                .build();
+    }
+}
