ff91cdfd138932610abb0eccd242e973fb4f00d6
Anata Arisa <kakata28419@gmail.com>
2023-10-23 03:34:37 +0700

    vinh/fetch course info and edit api

--- frontend/my-app/src/routes/instructor/course/edit/+page.svelte
+++ frontend/my-app/src/routes/instructor/course/edit/+page.svelte
@@ -14,6 +14,7 @@
     Label,
     Input,
   } from "flowbite-svelte";
+  import Editor from "@tinymce/tinymce-svelte";
 
   import type { LayoutData } from "../../../$types";
   import {
@@ -28,49 +29,89 @@
   } from "flowbite-svelte-icons";
   import SectionItem from "./SectionItem.svelte";
   import axios from "axios";
-  import { apiBaseUrl } from "../../../../service";
-    import type { EditCourse, EditCourseContent } from "$lib/types";
-    let editCourse = false;
-    let sessionName = "";
-    let courseContent: EditCourseContent = {
-      id: 1,
-      description: "", 
-      title: "",
-      videoLink: ""
-    }
+  import { GetCookie, apiBaseUrl } from "../../../../service";
+  import type { Course, EditCourse, EditCourseContent } from "$lib/types";
+  import { onMount } from "svelte";
+  let editCourse = false;
+  let sessionName = "";
+  let courseContent: EditCourseContent = {
+    id: 1,
+    description: "",
+    title: "",
+    videoLink: "",
+  };
   let course: EditCourse = {
     id: 1,
     description: "",
     durations: "3 weeks",
     name: "Nahida",
-    price: 500
-  } 
-  function ChangeMode(){
+    price: 500,
+  };
+  function ChangeMode() {
     editCourse = true;
-
   }
-  function ChangeView(){
+  function ChangeView() {
     editCourse = false;
   }
-  if (editCourse){
+  if (editCourse) {
+  }
+  function fetchCourseInfo(data: Course) {
+    course.id = data.id;
+    course.description = data.description;
+    course.durations = data.durations;
+    course.name = data.name;
+    course.price = data.price;
   }
   async function GetAllCourseData() {
     try {
       await axios
-        .get(apiBaseUrl + `course/all?id=${id}`)
-        .then((response) => {});
+        .get(apiBaseUrl + `course/all?id=${id}`, {
+          headers: {
+            Authorization: `Bearer ${GetCookie("USER")}`,
+          },
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            fetchCourseInfo(response.data.courseInfo);
+          }
+        });
     } catch (e) {}
   }
   async function ChangeCourseInfo() {
-    
+    try {
+      await axios
+        .put(apiBaseUrl + "course/edit", course, {
+          headers: {
+            Authorization: `Bearer ${GetCookie("USER")}`,
+          },
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            alert("Course updated");
+          }
+        });
+    } catch (e) {}
   }
-  async function ChangeSessionInfo() {
-    
-  }
+  async function ChangeSessionInfo() {}
   async function ChangeCourseDetailsInfo() {
-    
+    try {
+      axios
+        .put(apiBaseUrl + "/course-content/edit", courseContent, {
+          headers: {
+            Authorization: `Bearer ${GetCookie("USER")}`,
+          },
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            alert("Course Content updated");
+          }
+        });
+    } catch (e) {}
   }
   export let data: LayoutData;
+  onMount(() => {
+    GetAllCourseData();
+  });
 </script>
 
 <div class="grid grid-cols-4 gap-4 mt-10">
@@ -86,141 +127,216 @@
   <div class="p-4 col-span-3">
     <div class="w-full">
       <div class="bg-slate-600">
-        <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4 w-full h-96">
+        <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4 w-full h-12/12">
           <!--Left-->
           <div class="col-span-1 sm:col-span-3 h-56">
-            <img
-              class="items-center w-full"
-              alt=""
-              src=""
-            />
+            <img class="items-center w-full" alt="" src="" />
           </div>
           <!--Right-->
           {#if !editCourse}
             <div class="col-span-4 sm:col-span-9 h-full">
-            <div class="bg-white shadow rounded-lg p-6 h-full">
-              <h1 class="text-2xl font-bold mb-4">Course Information <span><Button color="alternative" size="xs" on:click={ChangeMode}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-              </svg>
-              </Button></span></h1> 
-              <br />
-              <!--1-->
-              <div class="grid grid-cols-2 gap-4">
-                <div id="viewmode">
-                  <div>Course ID</div>
-                  <div class="font-medium">01</div>
-                </div>
-                <div id="editmode" hidden>
-                  <Label class="space-y-2">
-                    <span>Course ID</span>
-                    <Input type="text" bind:value={course.id} size="sm" />
-                  </Label>
-                </div>
-                <div id="viewmode">
-                  <div>Course Name</div>
-                  <div class="font-medium">Loli Course For Beginner</div>
-                </div>
-                <div id="editmode" hidden>
-                  <Label class="space-y-2">
-                    <span>Course Name</span>
-                    <Input type="text" bind:value={course.name} size="sm" />
-                  </Label>
+              <div class="bg-white shadow rounded-lg p-6 h-full">
+                <h1 class="text-2xl font-bold mb-4">
+                  Course Information <span
+                    ><Button color="alternative" size="xs" on:click={ChangeMode}
+                      ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                        />
+                      </svg>
+                    </Button></span
+                  >
+                </h1>
+                <br />
+                <!--1-->
+                <div class="grid grid-cols-2 gap-4">
+                  <div id="viewmode">
+                    <div>Course ID</div>
+                    <div class="font-medium">01</div>
+                  </div>
+                  <div id="editmode" hidden>
+                    <Label class="space-y-2">
+                      <span>Course ID</span>
+                      <Input type="text" bind:value={course.id} size="sm" />
+                    </Label>
+                  </div>
+                  <div id="viewmode">
+                    <div>Course Name</div>
+                    <div class="font-medium">Loli Course For Beginner</div>
+                  </div>
+                  <div id="editmode" hidden>
+                    <Label class="space-y-2">
+                      <span>Course Name</span>
+                      <Input type="text" bind:value={course.name} size="sm" />
+                    </Label>
+                  </div>
+                  <div>
+                    <div>Price</div>
+                    <input
+                      disabled
+                      id="editinput"
+                      type="number"
+                      bind:value={course.price}
+                      class="font-medium border-none"
+                    />
+                  </div>
+                  <div>
+                    <div>Duration</div>
+                    <input
+                      disabled
+                      id="editinput"
+                      type="text"
+                      bind:value={course.durations}
+                      class="font-medium border-none pl-0 ml-0"
+                    />
+                  </div>
                 </div>
                 <div>
-                  <div>Price</div>
-                  <input disabled id="editinput"
-                    type="number"
-                    bind:value={course.price}
-                    class="font-medium border-none"
+                  <div>Description</div>
+                  <Editor
+                    apiKey="7gq4en8n7fdu1f8nqawf1uqup1tx0smi6v7dc3v1tf3dnhwa"
+                    channel="5-dev"
+                    modelEvents="input change undo redo"
+                    text="readonly-text-output"
+                    bind:value={course.description}
+                    disabled
                   />
+                  <!-- <textarea
+                    disabled
+                    bind:value={course.description}
+                    id="editinput"
+                    rows="4"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Write your thoughts here..."
+                  /> -->
                 </div>
-                <div>
-                  <div>Duration</div>
-                  <input disabled id="editinput"
-                    type="text"
-                    bind:value={course.durations}
-                    class="font-medium border-none pl-0 ml-0"
-                  />
-                </div>
+                <div />
               </div>
-              <div>
-                <div>Description</div>
-                <textarea disabled bind:value={course.description} id="editinput" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-              </div>
-              <div /> 
-            </div>
 
-            <!-- -->
-          </div>
+              <!-- -->
+            </div>
           {/if}
-          
+
           {#if editCourse}
             <div class="col-span-4 sm:col-span-9 h-full">
-            <div class="bg-white shadow rounded-lg p-6 h-full">
-              <h1 class="text-2xl font-bold mb-4">Course Information <span><Button color="alternative" size="xs" on:click={ChangeView}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-              </svg>
-              </Button></span></h1> 
-              <br />
-              <!--1-->
-              <div class="grid grid-cols-2 gap-4">
-                <div id="editmode">
-                  <Label class="space-y-2">
-                    <span>Course ID</span>
-                    <Input type="text" bind:value={course.id} size="sm" />
-                  </Label>
-                </div>
-                <div id="editmode">
-                  <Label class="space-y-2">
-                    <span>Course Name</span>
-                    <Input type="text" bind:value={course.name} size="sm" />
-                  </Label>
+              <div class="bg-white shadow rounded-lg p-6 h-full">
+                <h1 class="text-2xl font-bold mb-4">
+                  Course Information <span
+                    ><Button color="alternative" size="xs" on:click={ChangeView}
+                      ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                        />
+                      </svg>
+                    </Button></span
+                  >
+                </h1>
+                <br />
+                <!--1-->
+                <div class="grid grid-cols-2 gap-4">
+                  <div id="editmode">
+                    <Label class="space-y-2">
+                      <span>Course ID</span>
+                      <Input type="text" bind:value={course.id} size="sm" />
+                    </Label>
+                  </div>
+                  <div id="editmode">
+                    <Label class="space-y-2">
+                      <span>Course Name</span>
+                      <Input type="text" bind:value={course.name} size="sm" />
+                    </Label>
+                  </div>
+                  <div>
+                    <div>Price</div>
+                    <input
+                      id="editinput"
+                      type="number"
+                      bind:value={course.price}
+                      class="font-medium border-none"
+                    />
+                  </div>
+                  <div>
+                    <div>Duration</div>
+                    <input
+                      id="editinput"
+                      type="text"
+                      bind:value={course.durations}
+                      class="font-medium border-none pl-0 ml-0"
+                    />
+                  </div>
                 </div>
                 <div>
-                  <div>Price</div>
-                  <input id="editinput"
-                    type="number"
-                    bind:value={course.price}
-                    class="font-medium border-none"
+                  <div>Description</div>
+                  <Editor
+                    apiKey="7gq4en8n7fdu1f8nqawf1uqup1tx0smi6v7dc3v1tf3dnhwa"
+                    channel="5-dev"
+                    modelEvents="input change undo redo"
+                    text="readonly-text-output"
+                    bind:value={course.description}
                   />
+                  <!-- <textarea
+                    bind:value={course.description}
+                    id="editinput"
+                    rows="4"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Write your thoughts here..."
+                  /> -->
                 </div>
-                <div>
-                  <div>Duration</div>
-                  <input id="editinput"
-                    type="text"
-                    bind:value={course.durations}
-                    class="font-medium border-none pl-0 ml-0"
-                  />
-                </div>
+                <Button pill color="red" on:click={ChangeCourseInfo}
+                  >Save</Button
+                >
+                <div />
               </div>
-              <div>
-                <div>Description</div>
-                <textarea bind:value={course.description} id="editinput" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-              </div>
-              <Button pill color="red" on:submit={ChangeCourseInfo}>Save</Button>
-              <div />
+              <!-- -->
             </div>
-            <!-- -->
-          </div>
           {/if}
-          
         </div>
       </div>
       <div class="h-screen w-full border-spacing-1 mt-10">
-        <h1 class="text-2xl font-bold mb-4">Content Information</h1> 
+        <h1 class="text-2xl font-bold mb-4">Content Information</h1>
         <form class="w-full max-w-sm">
           <div>Session Name</div>
           <div class="flex items-center border-b border-teal-500 py-2">
-            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder=" " aria-label="Full name">
-            <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+            <input
+              class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              placeholder=" "
+              aria-label="Full name"
+            />
+            <button
+              class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="button"
+            >
               Save
             </button>
-            <button on:submit={ChangeSessionInfo} class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="reset">
+            <button
+              on:submit={ChangeSessionInfo}
+              class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+              type="reset"
+            >
               Cancel
             </button>
           </div>
         </form>
-        <br>
+        <br />
         <form>
           <div class="relative z-0 w-full mb-6 group">
             <input
@@ -241,7 +357,13 @@
           <div class="relative z-0 w-full mb-6 group">
             <div>
               <div>Description</div>
-              <textarea bind:value={courseContent.description} id="editinput" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+              <textarea
+                bind:value={courseContent.description}
+                id="editinput"
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your thoughts here..."
+              />
             </div>
           </div>
           <div class="relative z-0 w-full mb-6 group">
@@ -326,7 +448,7 @@
             </div>
           </div> -->
           <button
-          on:submit={ChangeCourseDetailsInfo}
+            on:submit={ChangeCourseDetailsInfo}
             type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >Submit</button
