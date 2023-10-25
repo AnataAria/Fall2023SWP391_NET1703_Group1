e7a9ab405175ef72621946562d8c7edbe9a6d7dc
Anata Arisa <kakata28419@gmail.com>
2023-10-26 01:56:15 +0700

    vinh/add new props for error message

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/exception/ExistedUserException.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/exception/ExistedUserException.java
@@ -3,6 +3,7 @@ package com.group1.drawingcouseselling.exception;
 import com.group1.drawingcouseselling.model.dto.ErrorMessage;
 import org.springframework.http.HttpStatus;
 
+import java.util.Date;
 import java.util.List;
 
 public class ExistedUserException extends BaseException{
@@ -11,6 +12,7 @@ public class ExistedUserException extends BaseException{
         this.errorMessage = ErrorMessage.builder()
                 .status(HttpStatus.BAD_REQUEST)
                 .errorList(List.of(message.split(",")))
+                .currentTimeError(new Date(System.currentTimeMillis()))
                 .build();
     }
 }
