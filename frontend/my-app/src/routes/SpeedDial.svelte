20fe8564df2606d9d5a84bdca398a50ea26a7a87
Anata Arisa <kakata28419@gmail.com>
2023-10-26 23:09:13 +0700

    vinh/fix error on fetching data

--- frontend/my-app/src/routes/SpeedDial.svelte
+++ frontend/my-app/src/routes/SpeedDial.svelte
@@ -1,30 +1,31 @@
 <script lang="ts">
-    import CreateCourseSection from "./CreateCourseSection.svelte";
-    import {
-        SpeedDial,
-        Listgroup,
-        ListgroupItem,
-        SpeedDialButton,
-    } from "flowbite-svelte";
-    import {
-        PenSolid,
-        ShareNodesSolid,
-        PrintSolid,
-        DownloadSolid,
-        FileCopySolid,
-        PlusSolid,
-    } from "flowbite-svelte-icons";
+  import CreateCourseSection from "./CreateCourseSection.svelte";
+  import {
+    SpeedDial,
+    Listgroup,
+    ListgroupItem,
+    SpeedDialButton,
+  } from "flowbite-svelte";
+  import {
+    PenSolid,
+    ShareNodesSolid,
+    PrintSolid,
+    DownloadSolid,
+    FileCopySolid,
+    PlusSolid,
+  } from "flowbite-svelte-icons";
+  export let id: number;
 </script>
 
 <SpeedDial
-    color="purpleToPink"
-    gradient
-    defaultClass="absolute right-0 bottom-6 text-left"
+  color="purpleToPink"
+  gradient
+  defaultClass="absolute right-0 bottom-6 text-left"
 >
-    <SpeedDialButton
-        name="Add"
-        class="rounded-full border-4 border-gray-300 text-left"
-    >
-        <CreateCourseSection />
-    </SpeedDialButton>
+  <SpeedDialButton
+    name="Add"
+    class="rounded-full border-4 border-gray-300 text-left"
+  >
+    <CreateCourseSection {id} />
+  </SpeedDialButton>
 </SpeedDial>
