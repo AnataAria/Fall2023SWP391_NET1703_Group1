757d5ce66f93743e464c11cb6e7793488ece67aa
hoangbaohuy <144062987+hoangbaohuy@users.noreply.github.com>
2023-10-26 12:58:53 +0700

    huy/add revenue and browse course and sidebar

---
+++ frontend/my-app/src/routes/staff/browsecourses/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import BrowseCourses from "../../BrowseCourses.svelte";
+    import SideBarAdmin from "../../SideBarAdmin.svelte";
+</script>
+<div class="grid grid-cols-12">
+    <div class="col-span-1">
+        <SideBarAdmin/>
+    </div>
+    <div class="col-span-11">
+        <BrowseCourses/>
+    </div>
+</div>
