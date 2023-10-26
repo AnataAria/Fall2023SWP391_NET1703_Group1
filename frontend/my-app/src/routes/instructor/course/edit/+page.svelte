cd93554c743dbe455d089c8815a6a3f8ec22e62b
thanlong421999 <thanlong421999@gmail.com>
2023-10-26 21:36:08 +0700

    ha/fix page instructor/course/edit ui

--- frontend/my-app/src/routes/instructor/course/edit/+page.svelte
+++ frontend/my-app/src/routes/instructor/course/edit/+page.svelte
@@ -65,10 +65,11 @@
   import { GetCookie, apiBaseUrl } from "../../../../service";
   import type { Course, EditCourse, EditCourseContent } from "$lib/types";
   import { onMount } from "svelte";
+  import CreateCourseSection from "../../../CreateCourseSection.svelte";
   let editCourse = false;
   let sessionName = "";
-    let deleteSessionModal = false;
-    let deleteCourseContentModal = false;
+  let deleteSessionModal = false;
+  let deleteCourseContentModal = false;
   let courseContent: EditCourseContent = {
     id: 1,
     description: "",
@@ -145,10 +146,8 @@
         });
     } catch (e) {}
   }
-  async function DeleteSession() {
-  }
-  async function DeleteCourseContent() {
-  }
+  async function DeleteSession() {}
+  async function DeleteCourseContent() {}
   export let data: LayoutData;
   onMount(() => {
     GetAllCourseData();
@@ -179,7 +178,11 @@
               <div class="bg-white shadow rounded-lg p-6 h-full">
                 <h1 class="text-2xl font-bold mb-4">
                   Course Information <span
-                    ><Button color="alternative" size="xs" on:click={ChangeMode}
+                    ><Button
+                      color="alternative"
+                      class="border-none"
+                      size="xs"
+                      on:click={ChangeMode}
                       ><svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -191,7 +194,7 @@
                         <path
                           stroke-linecap="round"
                           stroke-linejoin="round"
-                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
                         />
                       </svg>
                     </Button></span
@@ -272,7 +275,11 @@
               <div class="bg-white shadow rounded-lg p-6 h-full">
                 <h1 class="text-2xl font-bold mb-4">
                   Course Information <span
-                    ><Button color="alternative" size="xs" on:click={ChangeView}
+                    ><Button
+                      color="alternative"
+                      class="border-none"
+                      size="xs"
+                      on:click={ChangeView}
                       ><svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -284,7 +291,7 @@
                         <path
                           stroke-linecap="round"
                           stroke-linejoin="round"
-                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
                         />
                       </svg>
                     </Button></span
@@ -351,18 +358,46 @@
         </div>
       </div>
       <div class="h-screen w-full border-spacing-1 mt-10">
-        <h1 class="text-2xl font-bold mb-4">Session Information <span><Button on:click={() => (deleteSessionModal = true)} color="red" size="xs"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-        </svg>
-        </Button>
-          <Modal bind:open={deleteSessionModal} size="xs" autoclose>
-            <div class="text-center">
-              <ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
-              <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this session?</h3>
-              <Button color="red" class="mr-2" on:click={DeleteSession}>Yes, I'm sure</Button>
-              <Button color="alternative">No, cancel</Button>
-            </div>
-          </Modal></span></h1>
+        <CreateCourseSection {id} />
+        <h1 class="text-2xl font-bold mb-4">
+          Session Information <span
+            ><Button
+              on:click={() => (deleteSessionModal = true)}
+              color="red"
+              size="xs"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+            </Button>
+            <Modal bind:open={deleteSessionModal} size="xs" autoclose>
+              <div class="text-center">
+                <ExclamationCircleOutline
+                  class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                />
+                <h3
+                  class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400"
+                >
+                  Are you sure you want to delete this session?
+                </h3>
+                <Button color="red" class="mr-2" on:click={DeleteSession}
+                  >Yes, I'm sure</Button
+                >
+                <Button color="alternative">No, cancel</Button>
+              </div>
+            </Modal></span
+          >
+        </h1>
         <form class="w-full max-w-sm">
           <div>Session Name</div>
           <div class="flex items-center border-b border-teal-500 py-2">
@@ -388,19 +423,46 @@
           </div>
         </form>
         <br />
-        <h1 class="text-2xl font-bold mb-4">Content Information <span><Button on:click={() => (deleteCourseContentModal = true)} color="red" size="xs"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-        </svg>
-        </Button>
+        <h1 class="text-2xl font-bold mb-4">
+          Content Information <span
+            ><Button
+              on:click={() => (deleteCourseContentModal = true)}
+              color="red"
+              size="xs"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+            </Button>
 
-          <Modal bind:open={deleteCourseContentModal} size="xs" autoclose>
-            <div class="text-center">
-              <ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
-              <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this course content?</h3>
-              <Button color="red" class="mr-2" on:click={DeleteCourseContent}>Yes, I'm sure</Button>
-              <Button color="alternative">No, cancel</Button>
-            </div>
-          </Modal></span></h1> 
+            <Modal bind:open={deleteCourseContentModal} size="xs" autoclose>
+              <div class="text-center">
+                <ExclamationCircleOutline
+                  class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                />
+                <h3
+                  class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400"
+                >
+                  Are you sure you want to delete this course content?
+                </h3>
+                <Button color="red" class="mr-2" on:click={DeleteCourseContent}
+                  >Yes, I'm sure</Button
+                >
+                <Button color="alternative">No, cancel</Button>
+              </div>
+            </Modal></span
+          >
+        </h1>
         <form>
           <div class="relative z-0 w-full mb-6 group">
             <input
@@ -528,11 +590,3 @@
     </div>
   </div>
 </div>
-<SpeedDial defaultClass="absolute right-6 bottom-6">
-  <SpeedDialButton name="Add Section">
-    <ShareNodesSolid class="w-5 h-5" />
-  </SpeedDialButton>
-  <SpeedDialButton name="Add Course Content">
-    <PrintSolid class="w-5 h-5" />
-  </SpeedDialButton>
-</SpeedDial>
