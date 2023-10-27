cf189c6c69284e00005489e848ffa79c007e067d
Anata Arisa <kakata28419@gmail.com>
2023-10-28 02:45:27 +0700

    vinh/fix bug on submit register instructor

--- frontend/my-app/src/routes/InstructorLogin.svelte
+++ frontend/my-app/src/routes/InstructorLogin.svelte
@@ -389,6 +389,7 @@
                     </div>
                 </div>
                 <button
+                    on:click={handleRegister}
                     type="submit"
                     id="submitButton"
                     class="w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
@@ -396,7 +397,6 @@
                 >
                 <div id="loader" hidden>
                     <Button
-                        on:click={handleRegister}
                         color="red"
                         class="flex flex-wrap items-center gap-2 w-full"
                     >
