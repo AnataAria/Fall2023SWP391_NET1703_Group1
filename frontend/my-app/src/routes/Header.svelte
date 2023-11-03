fb5da6dd6bcf5b58bc3cf5ee48d0becb314d0dca
Anata Arisa <kakata28419@gmail.com>
2023-11-03 17:08:24 +0700

    vinh/add header certificate path

--- frontend/my-app/src/routes/Header.svelte
+++ frontend/my-app/src/routes/Header.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { DarkMode } from 'flowbite-svelte';
+  import { DarkMode } from "flowbite-svelte";
 
-  let btnClass = 'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2';
+  let btnClass =
+    "text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2";
   import {
     Navbar,
     NavBrand,
@@ -57,34 +58,37 @@
   let jwtData = {
     email: "",
     role: "",
-    createDate:"",
-    isActive:false
-  }
+    createDate: "",
+    isActive: false,
+  };
 
-  async function UserInfo(){
+  async function UserInfo() {
     const token = GetCookie("USER");
-  if(token!= null){
-    let header = {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-      "Authorization": `Bearer ${token}`
+    if (token != null) {
+      let header = {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      };
+      await axios
+        .get(apiBaseUrl + "account", {
+          headers: header,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            jwtData = res.data;
+            console.log(jwtData);
+            console.log(jwtData.role);
+          }
+        });
     }
-    await axios.get(apiBaseUrl + 'account', {
-      headers: header
-    })
-    .then((res) => {
-      if(res.status === 200){
-        jwtData = res.data;
-        console.log(jwtData);
-        console.log(jwtData.role);
-      }
-    });
   }
-}
 </script>
 
 <!-- ======= Header ======= -->
-<Navbar class="px-2 sm:px-4 py-2.5 fixed  z-20 top-0 left-0 border-b md:flex md:justify-start">
+<Navbar
+  class="px-2 sm:px-4 py-2.5 fixed  z-20 top-0 left-0 border-b md:flex md:justify-start"
+>
   <NavBrand href="/">
     <img src={headerImage} class="mr-3 h-6 lg:h-20" alt="Ademy Logo" />
   </NavBrand>
@@ -110,51 +114,70 @@
   <NavUl>
     <NavLi href="/teaching">Assign Instructor</NavLi>
     <NavLi href="/mylearning">My Learning</NavLi>
-    <NavLi href="/cart">
-      Your Cart
-    </NavLi>
+    <NavLi href="/cart">Your Cart</NavLi>
   </NavUl>
   <div class="ml-20 w-20">
     {#if status === true}
-      <Avatar id="user-drop" src="" class="cursor-pointer"/>
+      <Avatar id="user-drop" src="" class="cursor-pointer" />
       <Dropdown triggeredBy="#user-drop">
         <DropdownHeader>
           <span class="block text-sm">User</span>
-          <span class="block truncate text-sm font-medium"
-            >{jwtData.email}</span
+          <span class="block truncate text-sm font-medium">{jwtData.email}</span
           >
         </DropdownHeader>
         {#if jwtData.role === "INSTRUCTOR"}
-        <DropdownItem on:click={()=>{
-          window.location.href = "/instructor";
-        }}>Profile</DropdownItem>
+          <DropdownItem
+            on:click={() => {
+              window.location.href = "/instructor";
+            }}>Profile</DropdownItem
+          >
         {:else}
-        <DropdownItem on:click={()=>{
-          window.location.href = "/profile";
-        }}>Profile</DropdownItem>
-        {/if}        
+          <DropdownItem
+            on:click={() => {
+              window.location.href = "/profile";
+            }}>Profile</DropdownItem
+          >
+        {/if}
         <DropdownItem>Settings</DropdownItem>
         <DropdownItem>Earnings</DropdownItem>
-        <DropdownItem on:click={()=>{
-          window.location.href = "/cart";
-        }}>Cart</DropdownItem>
-        <DropdownItem on:click={()=>{
-          window.location.href="/mylearning";
-        }}>My Learning</DropdownItem>
+        <DropdownItem
+          on:click={() => {
+            window.location.href = "/cart";
+          }}>Cart</DropdownItem
+        >
+        <DropdownItem
+          on:click={() => {
+            window.location.href = "/mylearning";
+          }}>My Learning</DropdownItem
+        >
+        <DropdownItem
+          on:click={() => {
+            window.location.href = "/certificate";
+          }}>My Certificates</DropdownItem
+        >
         <DropdownDivider />
-        <DropdownItem on:click={()=>{
-          Logout();
-          window.location.reload();
-        }}>Sign out</DropdownItem>
+        <DropdownItem
+          on:click={() => {
+            Logout();
+            window.location.reload();
+          }}>Sign out</DropdownItem
+        >
       </Dropdown>
     {:else}
       <div class="flex justify-center space-x-10 w-full">
-        <Button color="red" class="w-20" on:click={()=>{
-          loginLogoutHandler();
-        }}>Login</Button>
-        <Button color="alternative" on:click={()=>{
-          window.location.href="/register";
-        }}>Sign Up</Button>
+        <Button
+          color="red"
+          class="w-20"
+          on:click={() => {
+            loginLogoutHandler();
+          }}>Login</Button
+        >
+        <Button
+          color="alternative"
+          on:click={() => {
+            window.location.href = "/register";
+          }}>Sign Up</Button
+        >
       </div>
     {/if}
   </div>
