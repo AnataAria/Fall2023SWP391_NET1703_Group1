90beaad179ce5211f473b9cb610c3debdd96b877
Anata Arisa <kakata28419@gmail.com>
2023-11-07 01:26:21 +0700

    vinh/fix error code for exam get infomation

--- api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/exception/BaseException.java
+++ api/drawingcouseselling/src/main/java/com/group1/drawingcouseselling/exception/BaseException.java
@@ -1,6 +1,7 @@
 package com.group1.drawingcouseselling.exception;
 
 import com.group1.drawingcouseselling.model.dto.ErrorMessage;
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 @Getter
@@ -9,6 +10,10 @@ public class BaseException extends RuntimeException {
 
     public BaseException(String message) {
         super(message);
+    }
+    public BaseException(ErrorMessage errorMessage) {
+        super(errorMessage.getErrorList().toString());
+        this.errorMessage = errorMessage;
     }
 }
 
