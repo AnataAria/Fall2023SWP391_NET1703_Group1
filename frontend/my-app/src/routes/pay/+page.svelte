d29fb0b5788ec7aa195695c08830e5b2d783654d
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2023-11-16 15:50:51 +0700

    Disable price input

--- frontend/my-app/src/routes/pay/+page.svelte
+++ frontend/my-app/src/routes/pay/+page.svelte
@@ -270,6 +270,7 @@
                 >
                 <div>
                     <input
+                        disabled
                         bind:value={order.price}
                         class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
                         type="number"
